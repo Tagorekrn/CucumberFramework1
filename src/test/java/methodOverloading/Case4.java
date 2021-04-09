239019a183c4a93cb59956428c64115f7f2e3fb6
Ravindranath Tagore Kolusu <v-rakolu@microsoft.com>
2021-04-09 09:54:57 +0530

    test

--- src/test/java/methodOverloading/Case4.java
+++ src/test/java/methodOverloading/Case4.java
@@ -14,5 +14,6 @@ public class Case4 {
 		c4.m1(10.5f, 10);
 		//c4.m1(10, 10); //The method m1(int, float) is ambiguous for the type Case4
 		//c4.m1(10.5f, 10.5f); //The method m1(float, int) in the type Case4 is not applicable for the arguments (float, float)
+		//Test
 	}
 }
